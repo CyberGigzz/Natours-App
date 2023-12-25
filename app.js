74ab3f0a7378823941a3e18e0a98e9915404a254
Giga <gigakatamadze0@gmail.com>
2023-12-25 23:19:27 +0400

    added comment

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const reviewRouter = require("./routes/reviewRoutes");
 const viewRouter = require("./routes/viewRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 
+// Started app
 const app = express();
 
 app.set("view engine", "pug");
