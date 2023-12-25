e9d0f29661f949d813d0da206c71609c48f0ab46
Giga <gigakatamadze0@gmail.com>
2023-12-25 23:16:02 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+// const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+// const xssFilters = require('xss-filters');
+const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set security HTTP headers
+// app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// CORS
+app.use(cors());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(cookieParser());
+
+//  Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// const xssMiddleware = (req, res, next) => {
+//   req.body = xssFilters.inHTMLData(req.body);
+//   req.query = xssFilters.inHTMLData(req.query);
+//   req.params = xssFilters.inHTMLData(req.params);
+//   next();
+// };
+// app.use(xssMiddleware);
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+// 3) ROUTES
+app.use("/", viewRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/bookings", bookingRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+// 4) START SERVER
+
+module.exports = app;
---
+++ public/js/stripe.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import axios from "axios";
+import { showAlert } from "./alerts";
+// import { Stripe } from "stripe";
+// const stripe = Stripe(
+//   "pk_test_51OQFF9AUgTYYrnBZgahzqjOc8nEr2b2aBrEugjW1hYRcY4QigYHMlJ0pbXG2WArE9M3dSKLljC7DohH9AEc9BOek00nekW9Tce"
+// );
+import { loadStripe } from "@stripe/stripe-js";
+
+export const bookTour = async (tourId) => {
+  const stripe = await loadStripe(
+    "pk_test_51OQFF9AUgTYYrnBZgahzqjOc8nEr2b2aBrEugjW1hYRcY4QigYHMlJ0pbXG2WArE9M3dSKLljC7DohH9AEc9BOek00nekW9Tce"
+  );
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert("error", err);
+  }
+};
