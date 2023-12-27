f95179b6f71ab88f5697bc289f39f155f792f2a7
Giga <gigakatamadze0@gmail.com>
2023-12-27 13:40:05 +0400

    Added some error handling to fix the app

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,6 +62,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
+
+  if (!user) {
+    console.error(`No user found with email ${session.customer_email}`);
+    return;
+  }
+
   const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
   // display_items
